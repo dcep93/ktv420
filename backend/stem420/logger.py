affba6040eb25732031eff5b51d0b6f806ee13cf
Daniel Cepeda <dcep93@gmail.com>
2026-01-01 17:46:04 -0500

    x

--- backend/stem420/logger.py
+++ backend/stem420/logger.py
@@ -4,5 +4,10 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+class Vars:
+    all_logs: list[str] = []
+
+
 def log(msg: str) -> None:
+    Vars.all_logs.append(msg)
     logger.info(msg)
