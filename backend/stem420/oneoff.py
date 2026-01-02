d31310ce5053fa79ff23b5a0e8e317bb9d1971b5
Daniel Cepeda <dcep93@gmail.com>
2026-01-01 20:37:10 -0500

    x

--- backend/stem420/oneoff.py
+++ backend/stem420/oneoff.py
@@ -1,9 +1,10 @@
 from . import run_job
 
-payload = run_job.Request(
+request = run_job.Request(
     mp3_path="gs://stem420-bucket/_stem420/f67750ba78151f96a973b4e490f4a9f4/input/honey.mp3",
     output_path="gs://stem420-bucket/_stem420/f67750ba78151f96a973b4e490f4a9f4/output/",
 )
 
-run_job.run_job(payload)
-print("success")
+run_job._process_request(request)
+
+print("oneoff complete")
