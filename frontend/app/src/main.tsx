b9efd2c79ffc6a82c76d45ee5cefa9da76bbd63a
Daniel Cepeda <dcep93@gmail.com>
2026-05-02 15:27:16 -0400

    x

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import AppRoutes from "./app/AppRoutes";
+import sha from "./features/stems/metadata/sha.json";
+
+console.log("KTV420 SHA", sha);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
