34ed6f8c9fb571048bebea6816e1e6f7fa89cf93
Daniel Cepeda <dcep93@gmail.com>
2026-07-08 09:10:16 -0400

    Clear migrated GCR images after deploy

--- .github/workflows/deploy_to_cloud_run.sh
+++ .github/workflows/deploy_to_cloud_run.sh
@@ -117,6 +117,16 @@ gcloud beta run deploy "$SERVICE_NAME" \
   --liveness-probe httpGet.path=/health
 
 
+for MIGRATED_GCR_IMG_PATH in \
+  "us-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/us.gcr.io/stem420" \
+  "us-docker.pkg.dev/${GOOGLE_CLOUD_PROJECT}/us.gcr.io/stem420/backend"; do
+  gcloud artifacts docker images delete "$MIGRATED_GCR_IMG_PATH" \
+    --project "${GOOGLE_CLOUD_PROJECT}" \
+    --delete-tags \
+    --quiet \
+  || echo "No migrated GCR image found at $MIGRATED_GCR_IMG_PATH"
+done
+
 gcloud container images list-tags "$IMG_PATH" \
   --sort-by=TIMESTAMP \
   --format='get(digest)' \
